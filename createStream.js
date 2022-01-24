47ffc96c50c7935ebb877c5ccb3b6e8193e52c75
Limonado <56095632+Limonado@users.noreply.github.com>
2022-01-24 21:58:56 +0000

    Styling

--- createStream.js
+++ createStream.js
@@ -3,30 +3,30 @@ module.exports = function createStream(server) {
 	const hls = require('hls-server');
 	const fs = require('fs');
 	new hls(server, {
-			provider: {
-					exists: (req, cb) => {
-							const ext = req.url.split('.').pop();
+		provider: {
+			exists: (req, cb) => {
+				const ext = req.url.split('.').pop();
 
-							if (ext !== 'm3u8' && ext !== 'ts') {
-									return cb(null, true);
-							}
+				if (ext !== 'm3u8' && ext !== 'ts') {
+					return cb(null, true);
+				}
 
-							fs.access(__dirname + req.url, fs.constants.F_OK, function(err) {
-									if (err) {
-											console.log('File not exist');
-											return cb(null, false);
-									}
-									cb(null, true);
-							});
-					},
-					getManifestStream: (req, cb) => {
-							const stream = fs.createReadStream(__dirname + req.url);
-							cb(null, stream);
-					},
-					getSegmentStream: (req, cb) => {
-							const stream = fs.createReadStream(__dirname + req.url);
-							cb(null, stream);
+				fs.access(__dirname + req.url, fs.constants.F_OK, function(err) {
+					if (err) {
+							console.log('File not exist');
+							return cb(null, false);
 					}
+					cb(null, true);
+				});
+			},
+			getManifestStream: (req, cb) => {
+				const stream = fs.createReadStream(__dirname + req.url);
+				cb(null, stream);
+			},
+			getSegmentStream: (req, cb) => {
+				const stream = fs.createReadStream(__dirname + req.url);
+				cb(null, stream);
 			}
+		}
 	});
 }
--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const ffmpeg = require('fluent-ffmpeg');
 const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 const Stopwatch = require('statman-stopwatch');
 const ytpl = require('ytpl');
+const ffmpegOptions = [
+	'-profile:v baseline',
+	'-level 3.0',
+	'-start_number 0',
+	'-hls_time 10',
+	'-vcodec libx264',
+	'-crf 27',
+	'-preset ultrafast',
+	'-movflags +faststart',
+	'-vf scale=iw/2:-2',
+	'-s 380x180',
+	'-c:a copy',
+	'-hls_list_size 0',
+	'-f hls'
+];
 
 const createStream = require('./createStream.js');
 
@@ -26,121 +41,114 @@ let conversionIndex = 0
 var playlist = [];
 let index = -1;
 let maxtime = 0;
+const path = require('path');
+const directory = 'movies';
 
 const io = new Server(server);
 io.on("connection", (socket) => {  
-    console.log('connection');
-    socket.emit('updatePlaylist', playlist);
-    if (videoRunning) { socket.emit('play', currentlyPlaying, sw.read(), maxtime); }
+	console.log('connection');
+	socket.emit('updatePlaylist', playlist);
+	if (videoRunning) { socket.emit('play', currentlyPlaying, sw.read(), index, maxtime, playlist[index]); }
 });
 
 const sw = new Stopwatch();
 
-
 function cleanPath() {
-    const path = require('path');
-    const directory = 'movies';
-    fs.readdir(directory, (err, files) => {
-    if (err) throw err;
-    for (const file of files) {
-        fs.unlink(path.join(directory, file), err => {
-        if (err) throw err;
-        });
-    }
-    });
-}
-function startConversion(url) {
-    converting = true;
-		var ffmpegOptions = [
-			'-profile:v baseline',
-			'-level 3.0',
-			'-start_number 0',
-			'-hls_time 10',
-			'-hls_list_size 0',
-			'-f hls'
-    ];
-
-		//Convert ytdl stream to .m3u8
-    var stream = ytdl(url, {format: 'mp4' });
-    var proc = new ffmpeg({source: stream});
-    proc.setFfmpegPath(ffmpegInstaller.path);
-    var output = proc.addOptions(ffmpegOptions).output('movies/output'+conversionIndex+'.m3u8');
-
-    fs.watch('movies/', (curr, prev) => {
-			if (!videoRunning && fs.existsSync('movies/output'+conversionIndex+'.m3u8')) {
-				createStream(server);
-				sw.reset();
-				maxtime = playlist[conversionIndex].lengthSeconds;
-				videoRunning = true;
-				videoTimeout();
-				io.emit("play", 'movies/output'+index+'.m3u8', 0, index, maxtime);
-				sw.start();
-				currentlyPlaying = 'movies/output'+index+'.m3u8';
-			}
-    });
-
-    output.on('end', () => {
-			if (playlist[conversionIndex+1] == undefined) {
-				console.log('end'); 
-				playlist = [];
-				conversionIndex = 0;
-				converting = false;
-				return;
-			}else {
-				conversionIndex++;
-				startConversion(playlist[conversionIndex].video_url);
-				return;
-			}
-    }).run();
+	fs.readdir(directory, (err, files) => {
+	if (err) throw err;
+	for (const file of files) {
+			fs.unlink(path.join(directory, file), err => {
+			if (err) throw err;
+			});
+	}
+	});
 }
 
 function videoTimeout() {
-    setTimeout(() => {
-        index++;
-        sw.stop();
-				if (!converting) {
-					index = -1;
-					sw.reset();
-					videoRunning = false;
-					return;
-				}
-        if (fs.existsSync('movies/output'+index+'.m3u8') && currentlyPlaying != 'movies/output'+index+'.m3u8') {
-            createStream(server);
-            maxtime = playlist[index].lengthSeconds;
-            videoRunning = true;
-            videoTimeout();
-            sw.start();
-            io.emit("play", 'movies/output'+index+'.m3u8', index, maxtime);
-            currentlyPlaying = 'movies/output'+index+'.m3u8';
-        }else { videoRunning = false; }
-    }, maxtime*1000);
+	setTimeout(() => {
+		index++;
+		fs.readdir(directory, (err, files) => {
+			if (err) throw err;
+			for (const file of files) {
+				if (!file.includes('output'+(index-1))) continue;
+
+				fs.unlink(path.join(directory, file), err => {
+				if (err) throw err;
+				});
+			}
+		});
+		sw.stop();
+		if (playlist[index] == undefined) { videoRunning = false; return; }
+		if (playlist[index].converted) {
+			startVideoFromIndex()
+		}else { 
+			videoRunning = false;
+		}
+	}, maxtime*1000);
 }
 
-//Get playlist
-app.get('/getplaylist', (req, res) => {
-    res.send(playlist);
-});
+function startVideoFromIndex() {
+	createStream(server);
+	maxtime = playlist[index].lengthSeconds;
+	videoRunning = true;
+	videoTimeout();
+	sw.reset();
+	sw.start();
+	io.emit("play", 'movies/output'+index+'.m3u8', sw.read(), index, maxtime, playlist[index]);
+	currentlyPlaying = 'movies/output'+index+'.m3u8';
+}
+
+function startConversion(url) {
+	converting = true;
+
+	//Get stream
+	var stream = ytdl(url, {format: 'mp4' });
+	var proc = new ffmpeg({source: stream});
+	proc.setFfmpegPath(ffmpegInstaller.path);
+	var output = proc.addOptions(ffmpegOptions).output('movies/output'+conversionIndex+'.m3u8');
+
+	output.on('end', () => {
+		playlist[conversionIndex].converted = true;
+		if (playlist[conversionIndex+1] != undefined) {
+			conversionIndex++;
+			startConversion(playlist[conversionIndex].video_url);
+		}else { converting = false; }
+	}).run();
+
+	fs.watch('movies/', (curr, prev) => {
+		if (!videoRunning && fs.existsSync('movies/output'+conversionIndex+'.m3u8')) {
+			startVideoFromIndex();
+		}
+	});
+}
 
 async function addVideo(url) {
-    var stream = await ytdl(url, {format: 'mp4' });
-    stream.on('info', (info) => {
-        playlist[playlist.length] = info.videoDetails;
-        //console.log(info.videoDetails);
-        
-    io.emit('updatePlaylist', playlist, index);
-        if (index == -1) { 
-            index = 0; 
-            cleanPath();
-            startConversion(playlist[0].video_url);
-        }else if (!converting) {
-            startConversion(playlist[0].video_url);
-        }
-        stream.destroy();
-        return true;
-    });  
+	var stream = await ytdl(url, {format: 'mp4' });
+	stream.on('info', (info) => {
+		info.videoDetails.converted = false;
+		playlist[playlist.length] = info.videoDetails;
+		io.emit('updatePlaylist', playlist, index);
+
+		if (index == -1) { 
+			index = 0; 
+			cleanPath();
+			startConversion(playlist[0].video_url);
+		}else if (!converting) {
+			conversionIndex++;
+			startConversion(info.videoDetails.video_url);
+		}
+
+		stream.destroy();
+		return true;
+	});  
 }
 
 //Add video to list
 app.post('/addvideo', async (req, res) => {
     addVideo(req.body.url);
+});
+
+//Get playlist
+app.get('/getplaylist', (req, res) => {
+    res.send(playlist);
 });
